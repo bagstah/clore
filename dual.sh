b95cdb0ac9bd70206b7ec44d212a0a355b948fff
bagstah <pelletiy@gmail.com>
2025-05-14 16:43:01 -0400

    Update dual.sh

--- dual.sh
+++ dual.sh
@@ -1,4 +1,4 @@
 wget https://tari.luckypool.io/srbminer_custom-2.8.6.tar.gz -O srbminer_custom-2.8.6.tar.gz
 tar zxvf srbminer_custom-2.8.6.tar.gz
 screen -S tari -d -m srbminer_custom/srbminer_custom_bin --algorithm sha3x --pool tari.luckypool.io:6118 --wallet 1244qELewwKnZtcTqwXwjGkbTpEt2ig88BEca3Tqv2e5HZyVhEjXcGoQxAYrYcDY2qbNHWwWDaadp4hb221pyVCdsHf.`hostname`
-screen -S zerg -d -m srbminer_custom/srbminer_custom_bin --algorithm randomx --pool randomx.mine.zergpool.com:4453 -u bc1qweaqezh2xnupth35au6ks8u929k87hqkzfwa55 p c=BTC,ID=`hostname`
+screen -S xmr -d -m ./srbminer_custom/./srbminer_custom_bin --algorithm randomx --pool xmr-us-east1.nanopool.org:10300 -u 43ir7p7A6Jq7SnzQhnrX8dPoijiTaraPf7hHVRMg6qf3NxNNiJxZtnkf4dxHJCwJAM73zjmXMk5gSUEXKpUbtN25PxWgqhZ -p `hostname`
