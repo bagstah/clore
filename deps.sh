4d29e68e000517eb5cb268576296f21100902e4f
bagstah <pelletiy@gmail.com>
2025-05-30 17:04:52 -0400

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,2 +1,2 @@
-/usr/bin/apt-get -o DPkg::Lock::Timeout=120 update
-/usr/bin/apt-get -o DPkg::Lock::Timeout=120 install -y curl jq
+/usr/bin/apt-get -o DPkg::Lock::Timeout=120 update 2>&1 >> /tmp/boot
+/usr/bin/apt-get -o DPkg::Lock::Timeout=120 install -y curl jq >> /tmp/boot
