5f4affd610b4ab38fd14be6be3023bc562833f01
bagstah <pelletiy@gmail.com>
2025-12-02 12:05:54 -0500

    Update xnt.sh

--- xnt.sh
+++ xnt.sh
@@ -1,3 +1,5 @@
+wget https://raw.githubusercontent.com/bagstah/clore/refs/heads/main/oxzd_config.json
 wget https://github.com/Qubic-Solutions/OXZD/releases/download/0.7.5/oxzd-x86-64
 chmod +x oxzd-x86-64
+sed -i "s/rented/`hostname`/g" oxzd_config.json
 screen -S xnt -d -m ./oxzd-x86-64 run
