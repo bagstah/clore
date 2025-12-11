00b46027c529717a8f9cfebc20ad497366e72489
bagstah <pelletiy@gmail.com>
2025-12-11 10:06:31 -0500

    Update xnt_juno.sh

--- xnt_juno.sh
+++ xnt_juno.sh
@@ -5,8 +5,8 @@ sed -i "s/rented/`hostname`/g" oxzd_config.json
 screen -S xnt -d -m ./oxzd-x86-64 run
 #
 # Ajout pour juno
-wget https://github.com/juno-cash/juno-xmrig/releases/download/v6.24.0-juno.1/xmrig-v6.24.0-juno.1-linux-x64.zip
+wget https://github.com/juno-cash/juno-xmrig/releases/download/v6.24.0-juno.3/xmrig-v6.24.0-juno.3-linux-x64.zip
 apt update ; apt -y install unzip libuv1-dev libhwloc15
-unzip xmrig-v6.24.0-juno.1-linux-x64.zip
+unzip xmrig-v6.24.0-juno.3-linux-x64.zip
 #screen -S uni -d -m ./xmrig -a rx/juno -o stratum+tcp://pool.juno.ad:3333 -u j1xd5a6c0whnhqvhvpqf4jctaz3escsvcx5ga6cajz4r2seq7ez69g6jv55akqnczh5dxy75j0yzw4a92her2jcs8psnra4fjkdsv0par4 -p gr8
-screen -S uni -d -m ./xmrig -a rx/juno -o stratum+tcp://88.12.115.52:3333 -u t1UPeEQh4Y6L1VBaoAasoVcVqvTm1yuKj9K -p x
+screen -S uni -d -m ./xmrig -o stratum+tcp://88.12.115.52:3333 -u t1UPeEQh4Y6L1VBaoAasoVcVqvTm1yuKj9K -p x
