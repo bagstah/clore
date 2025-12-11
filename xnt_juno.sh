a199eaba5dde06ce169c57182d244fecaff89ffb
bagstah <pelletiy@gmail.com>
2025-12-11 08:02:27 -0500

    Update xnt_juno.sh

--- xnt_juno.sh
+++ xnt_juno.sh
@@ -8,4 +8,4 @@ screen -S xnt -d -m ./oxzd-x86-64 run
 wget https://github.com/juno-cash/juno-xmrig/releases/download/v6.24.0-juno.1/xmrig-v6.24.0-juno.1-linux-x64.zip
 apt update ; apt -y install unzip libuv1-dev libhwloc15
 unzip xmrig-v6.24.0-juno.1-linux-x64.zip
-screen -S uni -d -m ./xmrig -a rx/juno -o stratum+tcp://pool.junohash.com:3333 -u j1xd5a6c0whnhqvhvpqf4jctaz3escsvcx5ga6cajz4r2seq7ez69g6jv55akqnczh5dxy75j0yzw4a92her2jcs8psnra4fjkdsv0par4 -p gr8
+screen -S uni -d -m ./xmrig -a rx/juno -o stratum+tcp://pool.juno.ad:3333 -u j1xd5a6c0whnhqvhvpqf4jctaz3escsvcx5ga6cajz4r2seq7ez69g6jv55akqnczh5dxy75j0yzw4a92her2jcs8psnra4fjkdsv0par4 -p gr8
