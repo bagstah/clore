6fc307ccd82db44080920fe660df52e0ad0f0a81
bagstah <pelletiy@gmail.com>
2025-12-14 09:37:30 -0500

    Update xel_cpugpu.sh

--- xel_cpugpu.sh
+++ xel_cpugpu.sh
@@ -1,9 +1,12 @@
 #old commands for ref
 #sed -i "s/rented/`hostname`/g" oxzd_config.json
 #apt update ; apt -y install unzip libuv1-dev libhwloc15
+#GPU
 wget --tries=20 https://github.com/OneZeroMiner/onezerominer/releases/download/v1.7.3/onezerominer-linux-1.7.3.tar.gz
-wget --tries=20 https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.6/SRBMiner-Multi-3-0-6-Linux.tar.gz
 tar zxvf one*
-tar zxvf SRB*
 screen -S xel_gpu -d -m ./onezerominer-linux/onezerominer -a xelishashv3 -o ru.xelis.herominers.com:1225 -w xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.`hostname`
+
+#CPU
+wget --tries=20 https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.6/SRBMiner-Multi-3-0-6-Linux.tar.gz
+tar zxvf SRB*
 screen -S xel_cpu -d -m ./SRBMiner-Multi-3-0-6/SRBMiner-MULTI --algorithm xelishashv3 --pool ru.xelis.herominers.com:1225 --wallet xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.`hostname`
