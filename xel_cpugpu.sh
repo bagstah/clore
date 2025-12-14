a6b1dfff5f42cef22788d47321be3d149ef918d2
bagstah <pelletiy@gmail.com>
2025-12-14 09:55:20 -0500

    Update xel_cpugpu.sh

--- xel_cpugpu.sh
+++ xel_cpugpu.sh
@@ -4,9 +4,9 @@
 #GPU
 wget --tries=20 https://github.com/OneZeroMiner/onezerominer/releases/download/v1.7.3/onezerominer-linux-1.7.3.tar.gz
 tar zxvf one*
-screen -S xel_gpu -d -m ./onezerominer-linux/onezerominer -a xelishashv3 -o ru.xelis.herominers.com:1225 -w xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.`hostname`
+screen -S xel_gpu -d -m ./onezerominer-linux/onezerominer -a xelishashv3 -o ru.xelis.herominers.com:1225 -w xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.gpu_`hostname`
 
 #CPU
 wget --tries=20 https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.6/SRBMiner-Multi-3-0-6-Linux.tar.gz
 tar zxvf SRB*
-screen -S xel_cpu -d -m ./SRBMiner-Multi-3-0-6/SRBMiner-MULTI --algorithm xelishashv3 --pool ru.xelis.herominers.com:1225 --wallet xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.`hostname`
+screen -S xel_cpu -d -m ./SRBMiner-Multi-3-0-6/SRBMiner-MULTI --algorithm xelishashv3 --pool ru.xelis.herominers.com:1225 --wallet xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.cpu_`hostname`
