d555e91db86904f2f543adb053fca177da423277
bagstah <pelletiy@gmail.com>
2025-12-14 09:24:18 -0500

    Update xel_cpugpu.sh

--- xel_cpugpu.sh
+++ xel_cpugpu.sh
@@ -4,6 +4,8 @@
 
 wget --tries=20 https://github.com/OneZeroMiner/onezerominer/releases/download/v1.7.3/onezerominer-linux-1.7.3.tar.gz
 wget --tries=20 https://github.com/doktor83/SRBMiner-Multi/releases/download/3.0.6/SRBMiner-Multi-3-0-6-Linux.tar.gz
+tar zxvf one*
+tar zxvf SRB*
 chmod +x SR*
 chmod +x one*
 screen -S xel_gpu -d -m ./onezerominer -a xelishashv3 -o ru.xelis.herominers.com:1225 -w xel:wt0pjhfs0txd59p0axz0dm0eeg0dxdxkv7ceuhnr8faztdt0q48qqyh0xqd.`hostname`
