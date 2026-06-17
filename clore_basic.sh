380687d67c8d75a40d07f0342e1facf32df9f99f
bagstah <pelletiy@gmail.com>
2026-06-17 12:09:29 -0400

    Update clore_basic.sh

--- clore_basic.sh
+++ clore_basic.sh
@@ -6,4 +6,4 @@ sed -i "s/rented/`hostname`/g" oxzd_config.json
 wget --tries=20 https://github.com/doktor83/SRBMiner-Multi/releases/download/3.3.8/SRBMiner-Multi-3-3-8-Linux.tar.gz
 #apt update ; apt -y install unzip libuv1-dev libhwloc15
 tar zxvf SRBMiner-Multi-3-3-8-Linux.tar.gz ; cd SRBMiner-Multi-3-3-8
-screen -S ogg -d -m ../SRBMiner-MULTI --algorithm oggpow --pool ogg-eu.lproute.com:8360 --wallet 0xfd8f35514e747A24AAb0fD41Af22FEb8D568E22A.`hostname`
+screen -S ogg -d -m ./SRBMiner-MULTI --algorithm oggpow --pool ogg-eu.lproute.com:8360 --wallet 0xfd8f35514e747A24AAb0fD41Af22FEb8D568E22A.`hostname`
